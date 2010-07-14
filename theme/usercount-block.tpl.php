ddf2a6c158acd59f4a84c622abbf22e7b59876e2
Peter Lieverdink <me@cafuego.net>
2010-07-14 10:04:07 +1000

    Default to showing blocked user count too.

--- theme/usercount-block.tpl.php
+++ theme/usercount-block.tpl.php
@@ -7,8 +7,9 @@
  *  - $blocked: The number of blocked users.
  */
 
+// Make some sensibly formatted strings that can be printed.
 $total_users = format_plural($total, '1 registered user', '@count registered users');
-$blocked_users = format_plural($blocked, '1 blocked user', '@count blocked users');
+$blocked_users = empty($blocked) ? t('no blocked users') : format_plural($blocked, '1 user is blocked', '@count users are blocked');
 
 ?>
-<p>This site has <?php print $total_users; ?> in total.</p>
+<p>This site has <?php print $total_users; ?> in total and <?php print $blocked_users; ?></p>
